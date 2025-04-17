9e4b8c2ef22f9926896e5720e76f23f04f2b0505
Juan Manuel Zappalorto <juan.zappalorto@grupoceta.com.ar>
2025-04-17 17:19:50 -0300

    Push

--- tp_2/src/components/BotonCompra.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/BotonCompra.css";
-
-const BotonCompra = ({ onClick }) => (
-  <button className="boton-compra" onClick={onClick}>
-    Comprar
-  </button>
-);
-
-export default BotonCompra;
